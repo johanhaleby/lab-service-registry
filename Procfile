d7d304e90ed236b00447a32e771b36bf907520ee
Johan Haleby <johan.haleby@gmail.com>
2014-01-16 12:24:10 +0100

    Updated mongo configuration to work with Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -jar target/lab-service-registry-1.0-SNAPSHOT.jar --server.port=$PORT
+web: java $JAVA_OPTS -jar target/lab-service-registry-1.0-SNAPSHOT.jar --server.port=$PORT --mongo.connection.url="$MONGOHQ_URL"
--- src/main/java/com/jayway/serviceregistry/boot/MongoConfiguration.java
+++ src/main/java/com/jayway/serviceregistry/boot/MongoConfiguration.java
@@ -2,26 +2,57 @@ package com.jayway.serviceregistry.boot;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.util.StringUtils;
 
+import java.net.UnknownHostException;
+
+import static com.mongodb.ServerAddress.defaultHost;
+
+/**
+ * Spring MongoDB automagically (using MongoRepositoriesAutoConfiguration) creates Mongo bean and MongoTemplate if not defined and @EnableMongoRepositories is used.
+ * However since Heroku defines a complete URI to Mongo (including username and pw)
+ * we need to override the Mongo bean creation instance and set to URI. But since Spring also creates a MongoTemplate which takes a database name as
+ * second parameter it overrides what's defined in the MongoClientURI. Thus we need to create the MongoClientURI as a Spring bean and use it's database name
+ * after having overridden Spring's MongoTemplate.
+ */
 @Configuration
-@EnableMongoRepositories
-class MongoConfiguration extends AbstractMongoConfiguration {
+@EnableMongoRepositories("com.jayway.serviceregistry.domain")
+class MongoConfiguration {
+    private static final Logger log = LoggerFactory.getLogger(MongoConfiguration.class);
 
-    @Override
-    protected String getDatabaseName() {
-        return "service-registry";
+    private static final String DEFAULT_DB_URI = "mongodb://127.0.0.1/service-registry";
+
+    @Value("${mongo.connection.url:" + DEFAULT_DB_URI + "}")
+    String mongoDbConnectionUri;
+
+    @Bean
+    public MongoClientURI mongoClientURI() {
+        final String uri;
+        if (StringUtils.isEmpty(mongoDbConnectionUri)) {
+            uri = defaultHost();
+        } else {
+            uri = mongoDbConnectionUri;
+        }
+        log.info("Mongo DB URL: {}", uri);
+        return new MongoClientURI(uri);
     }
 
-    @Override
+    @Bean
     public Mongo mongo() throws Exception {
-        return new MongoClient();
+        return new MongoClient(mongoClientURI());
     }
 
-    @Override
-    protected String getMappingBasePackage() {
-        return "com.jayway.serviceregistry.domain";
+    @Bean
+    MongoTemplate mongoTemplate(Mongo mongo) throws UnknownHostException {
+        return new MongoTemplate(mongo, mongoClientURI().getDatabase());
     }
+
 }
