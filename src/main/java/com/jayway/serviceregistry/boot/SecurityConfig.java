3f10534e47be64f2af3dd4ddeffbb06d5669615f
Johan Haleby <johan.haleby@gmail.com>
2014-01-20 14:02:40 +0100

    Cleanup openid auth

--- src/main/java/com/jayway/serviceregistry/boot/SecurityConfig.java
+++ src/main/java/com/jayway/serviceregistry/boot/SecurityConfig.java
@@ -1,10 +1,8 @@
 package com.jayway.serviceregistry.boot;
 
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
@@ -13,12 +11,10 @@ import org.springframework.security.core.userdetails.AuthenticationUserDetailsSe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.openid.OpenIDAuthenticationFilter;
 import org.springframework.security.openid.OpenIDAuthenticationToken;
 
 @Configuration
 @EnableWebMvcSecurity
-//@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,39 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .openidLogin() // Defined in OpenIDLoginConfigurer
                 .loginPage("/login.html")
                 .permitAll()
-                .authenticationUserDetailsService(new CustomUserDetailsService())
-                .attributeExchange("https://www.google.com/.*")
-                    .attribute("email")
-                        .type("http://axschema.org/contact/email")
-                        .required(true)
-                        .and()
-                    .attribute("firstname")
-                        .type("http://axschema.org/namePerson/first")
-                        .required(true)
-                        .and()
-                    .attribute("lastname")
-                        .type("http://axschema.org/namePerson/last")
-                        .required(true)
-                        .and()
-                    .and()
-                .attributeExchange(".*yahoo.com.*")
-                    .attribute("email")
-                        .type("http://axschema.org/contact/email")
-                        .required(true)
-                        .and()
-                    .attribute("fullname")
-                        .type("http://axschema.org/namePerson")
-                        .required(true)
-                        .and()
-                    .and()
-                .attributeExchange(".*myopenid.com.*")
-                    .attribute("email")
-                        .type("http://schema.openid.net/contact/email")
-                        .required(true)
-                        .and()
-                    .attribute("fullname")
-                        .type("http://schema.openid.net/namePerson")
-                        .required(true);
+                .authenticationUserDetailsService(new CustomUserDetailsService());
     }
 
     @Bean(name = "myAuthenticationManager")
