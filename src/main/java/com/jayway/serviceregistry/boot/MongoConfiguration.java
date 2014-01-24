837d1040ed376ff788f4b0fba9834e0d34c9d1e5
Johan Haleby <johan.haleby@gmail.com>
2014-01-24 08:52:32 +0100

    Revert "Simplified Mongo config" because it didn't work on Heroku
    This reverts commit e9b03fd4f16887ecd3ce558e63c899e3d350cbe4.

--- src/main/java/com/jayway/serviceregistry/boot/MongoConfiguration.java
+++ src/main/java/com/jayway/serviceregistry/boot/MongoConfiguration.java
@@ -1,23 +1,27 @@
 package com.jayway.serviceregistry.boot;
 
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.util.StringUtils;
 
-import javax.annotation.PostConstruct;
+import java.net.UnknownHostException;
 
 import static com.mongodb.ServerAddress.defaultHost;
-import static org.springframework.boot.autoconfigure.data.MongoRepositoriesAutoConfiguration.MongoProperties;
 
 /**
  * Spring MongoDB automagically (using MongoRepositoriesAutoConfiguration) creates Mongo bean and MongoTemplate if not defined and @EnableMongoRepositories is used.
- *
- * Note that we don't need to inject and configure the mongodb uri this way if we set the spring.data.mongo.uri property to the mongodb uri.
- * However Spring will then default to use the "test" database if the property is left out which is not want we want.
+ * However since Heroku defines a complete URI to Mongo (including username and pw)
+ * we need to override the Mongo bean creation instance and set to URI. But since Spring also creates a MongoTemplate which takes a database name as
+ * second parameter it overrides what's defined in the MongoClientURI. Thus we need to create the MongoClientURI as a Spring bean and use it's database name
+ * after having overridden Spring's MongoTemplate.
  */
 @Configuration
 @EnableMongoRepositories("com.jayway.serviceregistry.domain")
@@ -29,11 +33,8 @@ class MongoConfiguration {
     @Value("${mongo.connection.url:" + DEFAULT_DB_URI + "}")
     String mongoDbConnectionUri;
 
-    @Autowired
-    MongoProperties mongoProperties;
-
-    @PostConstruct
-    void configureMongoDbUri() {
+    @Bean
+    public MongoClientURI mongoClientURI() {
         final String uri;
         if (StringUtils.isEmpty(mongoDbConnectionUri)) {
             uri = defaultHost();
@@ -41,6 +42,17 @@ class MongoConfiguration {
             uri = mongoDbConnectionUri;
         }
         log.info("Mongo DB URL: {}", uri);
-        mongoProperties.setUri(mongoDbConnectionUri);
+        return new MongoClientURI(uri);
     }
+
+    @Bean
+    public Mongo mongo() throws Exception {
+        return new MongoClient(mongoClientURI());
+    }
+
+    @Bean
+    MongoTemplate mongoTemplate(Mongo mongo) throws UnknownHostException {
+        return new MongoTemplate(mongo, mongoClientURI().getDatabase());
+    }
+
 }
