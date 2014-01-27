a3227eb3621c37537a0d1e1494b1da069bd94253
Johan Haleby <johan.haleby@gmail.com>
2014-01-27 13:20:08 +0100

    Added more logging

--- src/main/java/com/jayway/serviceregistry/messagebus/ServiceMessageReceiver.java
+++ src/main/java/com/jayway/serviceregistry/messagebus/ServiceMessageReceiver.java
@@ -17,6 +17,8 @@ import java.util.regex.Pattern;
 
 import static com.jayway.serviceregistry.messagebus.protocol.EventType.SERVICE_OFFLINE_EVENT;
 import static com.jayway.serviceregistry.messagebus.protocol.EventType.SERVICE_ONLINE_EVENT;
+import static com.jayway.serviceregistry.messagebus.protocol.LogLevel.ERROR;
+import static com.jayway.serviceregistry.messagebus.protocol.LogLevel.INFO;
 import static java.lang.String.format;
 import static java.util.Arrays.asList;
 import static org.apache.commons.lang3.StringUtils.isBlank;
@@ -65,9 +67,11 @@ public class ServiceMessageReceiver {
                     Service service = toService(map);
                     String messageId = getStringOrLogError(map, MESSAGE_ID, format("%s with streamId %s is missing attribute '%s'.", SERVICE_ONLINE_EVENT, service.getServiceId(), MESSAGE_ID));
                     addServiceToRegistry(messageId, service);
+                    log(INFO, map, "Service " + service.getServiceId() + " registered.");
                     break;
                 case SERVICE_OFFLINE_EVENT:
-                    removeServiceFromRegistry(map);
+                    String serviceId = removeServiceFromRegistry(map);
+                    log(INFO, map, "Service " + serviceId + " unregistered.");
                     break;
                 default:
             }
@@ -76,16 +80,17 @@ public class ServiceMessageReceiver {
         }
     }
 
-    private void removeServiceFromRegistry(Map map) {
+    private String removeServiceFromRegistry(Map map) {
         String serviceId = getStringOrLogError(map, STREAM_ID, "Message didn't define the id of service to remove (streamId attribute is missing)");
         serviceRepository.delete(serviceId); // Deleting service that doesn't exist returns silently
+        return serviceId;
     }
 
     private void addServiceToRegistry(String messageId, Service service) {
         try {
             serviceRepository.save(service);
         } catch (DuplicateKeyException e) {
-            logError(service.getServiceId(), messageId, format("A service was already registered with name %s.", service.getDescription()));
+            log(LogLevel.ERROR, service.getServiceId(), messageId, format("A service was already registered with name %s.", service.getDescription()));
         }
     }
 
@@ -94,17 +99,17 @@ public class ServiceMessageReceiver {
         String streamId = getStringOrLogError(map, STREAM_ID, SERVICE_ONLINE_EVENT + " is missing attribute streamId.");
         if (!VALID_SERVICE_ID_PATTERN.matcher(streamId).matches()) {
             String errorMessage = format("The serviceId of event %s with streamId %s is must match reg exp %s.", SERVICE_ONLINE_EVENT, streamId, VALID_SERVICE_ID_PATTERN.toString());
-            logError(map, errorMessage);
+            log(LogLevel.ERROR, map, errorMessage);
             throw new ServiceMessageNotCorrectException(errorMessage);
         }
 
         Object potentialBody = map.get("body");
         if (potentialBody == null) {
-            logError(map, "Body was undefined in " + SERVICE_ONLINE_EVENT + " with streamId " + streamId);
+            log(LogLevel.ERROR, map, "Body was undefined in " + SERVICE_ONLINE_EVENT + " with streamId " + streamId);
         }
 
         if (!(potentialBody instanceof Map)) {
-            logError(map, "Body was not defined correctly in " + SERVICE_ONLINE_EVENT + " with streamId " + streamId);
+            log(LogLevel.ERROR, map, "Body was not defined correctly in " + SERVICE_ONLINE_EVENT + " with streamId " + streamId);
         }
 
         Map<String, Object> body = ((Map<String, Object>) potentialBody);
@@ -118,7 +123,7 @@ public class ServiceMessageReceiver {
 
         Object potentialMeta = getOrDefault(map, "meta", new HashMap<String, Object>());
         if (!(potentialMeta instanceof Map)) {
-            logError(map, format("The meta part of event %s with streamId %s is not defined correctly. Was %s but required is JSON object (Map).", SERVICE_ONLINE_EVENT, streamId, potentialMeta == null ? null : potentialMeta.getClass().getSimpleName()));
+            log(LogLevel.ERROR, map, format("The meta part of event %s with streamId %s is not defined correctly. Was %s but required is JSON object (Map).", SERVICE_ONLINE_EVENT, streamId, potentialMeta == null ? null : potentialMeta.getClass().getSimpleName()));
         }
         Map<String, Object> meta = (Map<String, Object>) potentialMeta;
 
@@ -137,24 +142,27 @@ public class ServiceMessageReceiver {
         return value;
     }
 
-    private void logError(Map map, String message) {
+    private void log(LogLevel logLevel, Map map, String message) {
         String streamId = toString(map, STREAM_ID);
         String messageId = toString(map, MESSAGE_ID);
-        logError(streamId, messageId, message);
+        log(logLevel, streamId, messageId, message);
     }
 
-    private void logError(String streamId, String messageId, String message) {
+    private void log(LogLevel logLevel, String streamId, String messageId, String message) {
         Map<String, Object> meta = new HashMap<>();
         meta.put(STREAM_ID, streamId == null ? UNKNOWN : streamId);
         meta.put(MESSAGE_ID, messageId == null ? UNKNOWN : messageId);
-        messageSender.sendMessage(Topic.LOG, Messages.logEvent(LogLevel.ERROR, SERVICE_REGISTRY, message, meta));
-        throw new ServiceMessageNotCorrectException(message);
+        messageSender.sendMessage(Topic.LOG, Messages.logEvent(logLevel, SERVICE_REGISTRY, message, meta));
+        log.debug("Sent log message '{}' with level {}.", message, logLevel);
+        if (logLevel == ERROR) {
+            throw new ServiceMessageNotCorrectException(message);
+        }
     }
 
     private String getStringOrLogError(Map map, String key, String errorMessage) {
         String string = toString(map, key);
         if (isBlank(string)) {
-            logError(map, errorMessage);
+            log(LogLevel.ERROR, map, errorMessage);
         }
         return string;
     }
@@ -167,7 +175,7 @@ public class ServiceMessageReceiver {
         if (value == null) {
             return null;
         } else if (!(value instanceof String)) {
-            logError(null, null, "The attribute " + key + " must be a String, was " + classOf(value) + ".");
+            log(LogLevel.ERROR, null, null, "The attribute " + key + " must be a String, was " + classOf(value) + ".");
         }
         return (String) value;
     }
